27a29c8e32f624938022d124862d1d10298e3211
Jesse Houchins <jesse.houchins@tstmedia.com>
2013-03-06 06:08:48 -0600

    add method alias support, more tests

--- tests/tests.js
+++ tests/tests.js
@@ -235,6 +235,20 @@ module('Number') /////////////////////////////
     ok(cast("not valid").number().invalid, "Fail when not a valid number.")
   })
 
+  test('lessThan', 4, function(){
+    ok(!cast("4.25").number().lessThan(4.4).outOfRange, "Less than another float.")
+    ok(cast(1.7).number().lessThan(1.7).outOfRange, "Less than same float.")
+    ok(!cast(1.7).number().lessThan(2).outOfRange, "Less than an integer.")
+    ok(!cast(1.7).number().lessThan("2").outOfRange, "Less than a numeric string.")
+  })
+
+  test('greaterThan', 4, function(){
+    ok(!cast("4.25").number().greaterThan(4.1).outOfRange, "Greater than another float.")
+    ok(cast(1.7).number().greaterThan(1.7).outOfRange, "Greater than same float.")
+    ok(!cast(1.7).number().greaterThan(1).outOfRange, "Greater than an integer.")
+    ok(!cast(1.7).number().greaterThan("1").outOfRange, "Greater than a numeric string.")
+  })
+
 
 module('Integer') /////////////////////////////
 
